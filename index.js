5f06b5da46e0344faee481cd3230c0d674d40123
danmousley367 <dan.mousley@softwire.com>
2022-04-26 17:06:38 +0100

    Part 2 complete

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ class Bus {
     }
 
     listBus = () => {
-        console.log(`The ${this.route} to ${this.destination} will arrive in ${this.timeToArrival} mins.`)
+        console.log(`- The ${this.route} to ${this.destination} will arrive in ${this.timeToArrival} mins.`)
     }
 }
 
@@ -23,22 +23,64 @@ const sortBuses = (busData) => {
     })
 }
 
-// 490008660N
-const busStopCode = readlineSync.question('Please enter your bus stop code: ')
+// NW5 1TL
+const postCode = readlineSync.question('Please enter your post code: ')
 
-axios.get(`https://api.tfl.gov.uk/StopPoint/${busStopCode}/Arrivals?app_id=${APP_ID}&app_key=${APP_KEY}`)
-    .then((response) => {
-        const sortedData = sortBuses(response.data)
-        const nextFiveBuses = sortedData.slice(0, 5)
-        const busTimes = []
-
-        nextFiveBuses.forEach((busData) => {
-            const bus = new Bus(busData.lineName, busData.destinationName, busData.timeToStation)
-            busTimes.push(bus)
+const getPostCodeData = (postCode) => {
+    axios.get(`https://api.postcodes.io/postcodes/${postCode}`)
+        .then((response) => {
+            getBusStopCodes([response.data.result.latitude, response.data.result.longitude])
         })
+        .catch((error) => {
+            console.log(error)
+        })
+}
 
-        busTimes.forEach((bus) => bus.listBus())
+getPostCodeData(postCode)
+
+const sortStopsByDistance = (stops) => {
+    return stops.sort((a, b) => {
+        return a.distance - b.distance
     })
-    .catch((error) => {
-        console.log(error)
-    })
+}
+
+const getBusStopCodes = (latAndLon) => {
+    const [lat, lon] = latAndLon
+
+    axios.get(`https://api.tfl.gov.uk/Stoppoint?lat=${lat}&lon=${lon}&stoptypes=NaptanBusCoachStation,NaptanPublicBusCoachTram&radius=500`)
+        .then((response) => {
+            const stopPoints = response.data.stopPoints
+            const sortedStops = sortStopsByDistance(stopPoints)
+            const nearestStops = sortedStops.slice(0, 2)
+
+            nearestStops.forEach(async (stop) => {
+                getBuses(stop.naptanId)
+                    .then((buses) => {
+                        console.log(`${stop.commonName} is a distance of ${stop.distance.toFixed(2)}m away. The next buses are:`)
+                        buses.forEach((bus) => bus.listBus())
+                    })
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+const getBuses = (busStopCodes) => {
+    return axios.get(`https://api.tfl.gov.uk/StopPoint/${busStopCodes}/Arrivals?app_id=${APP_ID}&app_key=${APP_KEY}`)
+        .then((response) => {
+            const sortedData = sortBuses(response.data)
+            const nextFiveBuses = sortedData.slice(0, 5)
+            const buses = []
+
+            nextFiveBuses.forEach((busData) => {
+                const bus = new Bus(busData.lineName, busData.destinationName, busData.timeToStation)
+                buses.push(bus)
+            })
+
+            return buses
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
