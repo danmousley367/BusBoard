3d89fc530939c2a58f5082a1d4501d0e01f83a07
danmousley367 <dan.mousley@softwire.com>
2022-04-26 12:31:29 +0100

    Allow user to enter bus stop code

--- index.js
+++ index.js
@@ -2,13 +2,43 @@ const axios = require('axios').default;
 
 const APP_ID = "tfl_key"
 const APP_KEY = "c918382153274eef95ca07f91a280c17"
-axios.get(`https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals?app_id=${APP_ID}&app_key=${APP_KEY}`)
+
+const readlineSync = require("readline-sync");
+
+class Bus {
+    constructor(route, destination, timeToArrival) {
+        this.route = route
+        this.destination = destination
+        this.timeToArrival = timeToArrival
+    }
+
+    listBus = () => {
+        console.log(`The ${this.route} to ${this.destination} will arrive in ${this.timeToArrival} mins.`)
+    }
+}
+
+const sortBuses = (busData) => {
+    return busData.sort((a, b) => {
+        return a.timeToStation - b.timeToStation
+    })
+}
+
+// 490008660N
+const busStopCode = readlineSync.question('Please enter your bus stop code: ')
+
+axios.get(`https://api.tfl.gov.uk/StopPoint/${busStopCode}/Arrivals?app_id=${APP_ID}&app_key=${APP_KEY}`)
     .then((response) => {
-        console.log(response)
+        const sortedData = sortBuses(response.data)
+        const nextFiveBuses = sortedData.slice(0, 5)
+        const busTimes = []
+
+        nextFiveBuses.forEach((busData) => {
+            const bus = new Bus(busData.lineName, busData.destinationName, busData.timeToStation)
+            busTimes.push(bus)
+        })
+
+        busTimes.forEach((bus) => bus.listBus())
     })
     .catch((error) => {
         console.log(error)
-    })
-    .then(() => {
-        // next function
     })
